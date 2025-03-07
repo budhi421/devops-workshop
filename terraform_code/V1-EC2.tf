4e4188110f662b14426d1ff7951959baad435ee2
budhiram sahani <budhiramsahani421@gmail.com>
2025-03-08 01:31:18 +0530

    commiting V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"  
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-05b10e08d247fb927"
     instance_type = "t2.micro"
     key_name = "dpp"
+  
 }
+
